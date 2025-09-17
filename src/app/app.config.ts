6664e7624631f46f8abe72f77717341321b88d5d
Ariel Paixão <arielpaixao10@gmail.com>
2025-09-17 19:53:36 -0300

    feat(interceptor): login.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const router = inject(Router); // Direciona para as telas.
+
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+
+      if (err.status == 401) {
+        // Token expirou
+        localStorage.clear(); // Limpa todos os dados do localstorage.
+        router.navigate(['/login']); // Redireciona para o login.
+      }
+
+      return throwError(() => err);
+
+    })
+  );
+
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(), // Instância o HttpModule
+    provideHttpClient(
+      withInterceptors([ authInterceptor ])
+    ), // Instância o HttpModule
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
